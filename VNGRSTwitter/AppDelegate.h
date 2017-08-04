3a656ee5bf88e904bdf8972f2a9de03286b7c54e
ilker Sen <erdemilker.sen@intertech.com.tr>
2017-08-04 16:18:32 +0300

    Initial Commit

---
+++ VNGRSTwitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VNGRSTwitter
+//
+//  Created by ilker Sen on 4.08.2017.
+//  Copyright © 2017 Erdem ilker Sen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VNGRSTwitter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VNGRSTwitter
+//
+//  Created by ilker Sen on 4.08.2017.
+//  Copyright © 2017 Erdem ilker Sen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
